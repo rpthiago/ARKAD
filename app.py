c05065643c7bec43194aa17b15c8287be6c3ea22
rpthiago <rp_thiago@yahoo.com.br>
2023-07-24 12:19:05 -0300

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import xlsxwriter
 
 st.set_page_config(
-    page_title='Teoria do Retornos - Grupo Arkad',
+    page_title='Teoria do Retornos - Grupo Arkad - FLASHSCORE',
 )
 
 def pagina_01():
@@ -19,7 +19,7 @@ def pagina_01():
 
     @st.cache
     def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Jogos_do_Dia.csv")
+        data_jogos = pd.read_csv(f"./Jogos_do_Dia/{dia}_Jogos_do_Dia.csv")
         
         return data_jogos
 
@@ -58,7 +58,7 @@ def pagina_02():
 
     @st.cache
     def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Teoria_dos_Retornos_Match_Odds.csv")
+        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Teoria_dos_Retornos_Match_Odds_arkad.csv")
         
         return data_jogos
 
@@ -83,93 +83,12 @@ def pagina_02():
     button = st.download_button(
         label='Download',
         data=download_excel(),
-        file_name=f'Teoria_dos_Retornos_Match_Odds_{dia}.xlsx',
+        file_name=f'Teoria_dos_Retornos_Match_Odds_arkad_{dia}.xlsx',
         mime='application/vnd.ms-excel')
     
 
 
 def pagina_03():
-    st.subheader("Teoria dos Retornos - Over/Under 0.5 FT")
-
-    dia = st.date_input(
-        "Data de Análise",
-        date.today())
-
-    ########## Importando os Jogos do Dia ##########
-
-    @st.cache
-    def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Teoria_dos_Retornos_Over_Under_05.csv")
-        
-        return data_jogos
-
-    df_jogos = load_data_jogos()
-
-    df_jogos.dropna(inplace=True)
-    df_jogos = df_jogos.reset_index(drop=True)
-    df_jogos.index += 1
-
-    st.table(df_jogos)
-
-    # Define a função que retorna a planilha em formato XLSX
-    def download_excel():
-        output = BytesIO()
-        writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        df_jogos.to_excel(writer, index=False, sheet_name='Sheet1')
-        writer.save()
-        processed_data = output.getvalue()
-        return processed_data
-
-    # Cria o botão de download
-    button = st.download_button(
-        label='Download',
-        data=download_excel(),
-        file_name=f'Teoria_dos_Retornos_Over_Under_05_{dia}.xlsx',
-        mime='application/vnd.ms-excel')
-    
-
-
-def pagina_04():
-    st.subheader("Teoria dos Retornos - Over/Under 1.5 FT")
-
-    dia = st.date_input(
-        "Data de Análise",
-        date.today())
-
-    ########## Importando os Jogos do Dia ##########
-
-    @st.cache
-    def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Teoria_dos_Retornos_Over_Under_15.csv")
-        
-        return data_jogos
-
-    df_jogos = load_data_jogos()
-
-    df_jogos.dropna(inplace=True)
-    df_jogos = df_jogos.reset_index(drop=True)
-    df_jogos.index += 1
-
-    st.table(df_jogos)
-
-    # Define a função que retorna a planilha em formato XLSX
-    def download_excel():
-        output = BytesIO()
-        writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        df_jogos.to_excel(writer, index=False, sheet_name='Sheet1')
-        writer.save()
-        processed_data = output.getvalue()
-        return processed_data
-
-    # Cria o botão de download
-    button = st.download_button(
-        label='Download',
-        data=download_excel(),
-        file_name=f'Teoria_dos_Retornos_Over_Under_15_{dia}.xlsx',
-        mime='application/vnd.ms-excel')
-    
-
-def pagina_05():
     st.subheader("Teoria dos Retornos - Over/Under 2.5 FT")
 
     dia = st.date_input(
@@ -180,7 +99,7 @@ def pagina_05():
 
     @st.cache
     def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Teoria_dos_Retornos_Over_Under_25.csv")
+        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Teoria_dos_Retornos_Over_Under_25_ARKAD.csv")
         
         return data_jogos
 
@@ -205,98 +124,12 @@ def pagina_05():
     button = st.download_button(
         label='Download',
         data=download_excel(),
-        file_name=f'Teoria_dos_Retornos_Over_Under_25_{dia}.xlsx',
-        mime='application/vnd.ms-excel')
-    
-
-    
-def pagina_06():
-    st.subheader("Teoria dos Retornos - Over/Under 3.5 FT")
-
-    dia = st.date_input(
-        "Data de Análise",
-        date.today())
-
-    ########## Importando os Jogos do Dia ##########
-
-    @st.cache
-    def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Teoria_dos_Retornos_Over_Under_35.csv")
-        
-        return data_jogos
-
-    df_jogos = load_data_jogos()
-
-    df_jogos.dropna(inplace=True)
-    df_jogos = df_jogos.reset_index(drop=True)
-    df_jogos.index += 1
-
-    st.table(df_jogos)
-
-    # Define a função que retorna a planilha em formato XLSX
-    def download_excel():
-        output = BytesIO()
-        writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        df_jogos.to_excel(writer, index=False, sheet_name='Sheet1')
-        writer.save()
-        processed_data = output.getvalue()
-        return processed_data
-
-    # Cria o botão de download
-    button = st.download_button(
-        label='Download',
-        data=download_excel(),
-        file_name=f'Teoria_dos_Retornos_Over_Under_35_{dia}.xlsx',
+        file_name=f'Teoria_dos_Retornos_Over_Under_25_ARKAD_{dia}.xlsx',
         mime='application/vnd.ms-excel')
     
 
 
-
-    
-def pagina_07():
-    st.subheader("Teoria dos Retornos - Over/Under 4.5 FT")
-
-    dia = st.date_input(
-        "Data de Análise",
-        date.today())
-
-    ########## Importando os Jogos do Dia ##########
-
-    @st.cache
-    def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Teoria_dos_Retornos_Over_Under_45.csv")
-        
-        return data_jogos
-
-    df_jogos = load_data_jogos()
-
-    df_jogos.dropna(inplace=True)
-    df_jogos = df_jogos.reset_index(drop=True)
-    df_jogos.index += 1
-
-    st.table(df_jogos)
-
-    # Define a função que retorna a planilha em formato XLSX
-    def download_excel():
-        output = BytesIO()
-        writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        df_jogos.to_excel(writer, index=False, sheet_name='Sheet1')
-        writer.save()
-        processed_data = output.getvalue()
-        return processed_data
-
-    # Cria o botão de download
-    button = st.download_button(
-        label='Download',
-        data=download_excel(),
-        file_name=f'Teoria_dos_Retornos_Over_Under_45_{dia}.xlsx',
-        mime='application/vnd.ms-excel')
-    
-
-
-    
-
-def pagina_08():
+def pagina_04():
     st.subheader("Teoria dos Retornos - Ambas Marcam")
 
     dia = st.date_input(
@@ -307,7 +140,7 @@ def pagina_08():
 
     @st.cache
     def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Teoria_dos_Retornos_BTTS.csv")
+        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_Teoria_dos_Retornos_BTTS_ARKAD.csv")
         
         return data_jogos
 
@@ -332,12 +165,171 @@ def pagina_08():
     button = st.download_button(
         label='Download',
         data=download_excel(),
-        file_name=f'Teoria_dos_Retornos_BTTS_{dia}.xlsx',
+        file_name=f'Teoria_dos_Retornos_BTTS_ARKAD_{dia}.xlsx',
         mime='application/vnd.ms-excel')
+    
 
+def pagina_05():
+    st.subheader("LAY AWAY NEW")
+
+    dia = st.date_input(
+        "Data de Análise",
+        date.today())
+
+    ########## Importando os Jogos do Dia ##########
+
+    @st.cache
+    def load_data_jogos():
+        data_jogos = pd.read_csv(f"./JOGOS/{dia}_lay_away_new.csv")
+        
+        return data_jogos
+
+    df_jogos = load_data_jogos()
+
+    df_jogos.dropna(inplace=True)
+    df_jogos = df_jogos.reset_index(drop=True)
+    df_jogos.index += 1
+
+    st.table(df_jogos)
+
+    # Define a função que retorna a planilha em formato XLSX
+    def download_excel():
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df_jogos.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.save()
+        processed_data = output.getvalue()
+        return processed_data
+
+    # Cria o botão de download
+    button = st.download_button(
+        label='Download',
+        data=download_excel(),
+        file_name=f'Lay_Away_New_{dia}.xlsx',
+        mime='application/vnd.ms-excel')
+    
+
+    
+def pagina_06():
+    st.subheader("Lay Goleada")
+
+    dia = st.date_input(
+        "Data de Análise",
+        date.today())
+
+    ########## Importando os Jogos do Dia ##########
+
+    @st.cache
+    def load_data_jogos():
+        data_jogos = pd.read_csv(f"./JOGOS/{dia}_lay_goleada.csv")
+        
+        return data_jogos
+
+    df_jogos = load_data_jogos()
+
+    df_jogos.dropna(inplace=True)
+    df_jogos = df_jogos.reset_index(drop=True)
+    df_jogos.index += 1
+
+    st.table(df_jogos)
+
+    # Define a função que retorna a planilha em formato XLSX
+    def download_excel():
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df_jogos.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.save()
+        processed_data = output.getvalue()
+        return processed_data
+
+    # Cria o botão de download
+    button = st.download_button(
+        label='Download',
+        data=download_excel(),
+        file_name=f'lay_goleada_{dia}.xlsx',
+        mime='application/vnd.ms-excel')
+    
+def pagina_07():
+    st.subheader("BTTS_YES")
+
+    dia = st.date_input(
+        "Data de Análise",
+        date.today())
+
+    ########## Importando os Jogos do Dia ##########
+
+    @st.cache
+    def load_data_jogos():
+        data_jogos = pd.read_csv(f"./JOGOS/{dia}_btts_yes.csv")
+        
+        return data_jogos
+
+    df_jogos = load_data_jogos()
+
+    df_jogos.dropna(inplace=True)
+    df_jogos = df_jogos.reset_index(drop=True)
+    df_jogos.index += 1
+
+    st.table(df_jogos)
+
+    # Define a função que retorna a planilha em formato XLSX
+    def download_excel():
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df_jogos.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.save()
+        processed_data = output.getvalue()
+        return processed_data
+
+    # Cria o botão de download
+    button = st.download_button(
+        label='Download',
+        data=download_excel(),
+        file_name=f'BTTS-yes_{dia}.xlsx',
+        mime='application/vnd.ms-excel')          
+    
+
+def pagina_08():
+    st.subheader("OVER 0,5 HT")
+
+    dia = st.date_input(
+        "Data de Análise",
+        date.today())
+
+    ########## Importando os Jogos do Dia ##########
+
+    @st.cache
+    def load_data_jogos():
+        data_jogos = pd.read_csv(f"./JOGOS/{dia}_over05ht.csv")
+        
+        return data_jogos
+
+    df_jogos = load_data_jogos()
+
+    df_jogos.dropna(inplace=True)
+    df_jogos = df_jogos.reset_index(drop=True)
+    df_jogos.index += 1
+
+    st.table(df_jogos)
+
+    # Define a função que retorna a planilha em formato XLSX
+    def download_excel():
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df_jogos.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.save()
+        processed_data = output.getvalue()
+        return processed_data
+
+    # Cria o botão de download
+    button = st.download_button(
+        label='Download',
+        data=download_excel(),
+        file_name=f'OVER05HT_{dia}.xlsx',
+        mime='application/vnd.ms-excel')
 
 def pagina_09():
-    st.subheader("LAY_0x1_Leandro")
+    st.subheader("OVER2_5")
 
     dia = st.date_input(
         "Data de Análise",
@@ -347,8 +339,8 @@ def pagina_09():
 
     @st.cache
     def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_LAY_0X1.csv")
-
+        data_jogos = pd.read_csv(f"./JOGOS/{dia}_over2_5.csv")
+        
         return data_jogos
 
     df_jogos = load_data_jogos()
@@ -372,11 +364,12 @@ def pagina_09():
     button = st.download_button(
         label='Download',
         data=download_excel(),
-        file_name=f'LAY_0X1_{dia}.xlsx',
-        mime='application/vnd.ms-excel')
-
+        file_name=f'OVER2_5_{dia}.xlsx',
+        mime='application/vnd.ms-excel')    
+    
+    
 def pagina_10():
-    st.subheader("LAY_1X0_Leandro")
+    st.subheader("LAY 0X1")
 
     dia = st.date_input(
         "Data de Análise",
@@ -384,19 +377,58 @@ def pagina_10():
 
     ########## Importando os Jogos do Dia ##########
 
-    @st.cache
-    def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Teoria_dos_Retornos/{dia}_LAY_1X0.csv")
+#     @st.cache
+#     def load_data_jogos():
+#         data_jogos = pd.read_csv(f"./JOGOS/{dia}_Lay_0x1.csv")
+        
+#         return data_jogos
 
-        return data_jogos
+#     df_jogos = load_data_jogos()
 
-    df_jogos = load_data_jogos()
+#     df_jogos.dropna(inplace=True)
+#     df_jogos = df_jogos.reset_index(drop=True)
+#     df_jogos.index += 1
 
-    df_jogos.dropna(inplace=True)
-    df_jogos = df_jogos.reset_index(drop=True)
-    df_jogos.index += 1
+#     st.table(df_jogos)
 
-    st.table(df_jogos)
+#     # Define a função que retorna a planilha em formato XLSX
+#     def download_excel():
+#         output = BytesIO()
+#         writer = pd.ExcelWriter(output, engine='xlsxwriter')
+#         df_jogos.to_excel(writer, index=False, sheet_name='Sheet1')
+#         writer.save()
+#         processed_data = output.getvalue()
+#         return processed_data
+
+#     # Cria o botão de download
+#     button = st.download_button(
+#         label='Download',
+#         data=download_excel(),
+#         file_name=f'Lay_0x1_{dia}.xlsx',
+#         mime='application/vnd.ms-excel')
+
+# def pagina_11():
+#     st.subheader("LAY 1X0")
+
+#     dia = st.date_input(
+#         "Data de Análise",
+#         date.today())
+
+#     ########## Importando os Jogos do Dia ##########
+
+#     @st.cache
+#     def load_data_jogos():
+#         data_jogos = pd.read_csv(f"./JOGOS/{dia}_Lay_1x0.csv")
+        
+#         return data_jogos
+
+#     df_jogos = load_data_jogos()
+
+#     df_jogos.dropna(inplace=True)
+#     df_jogos = df_jogos.reset_index(drop=True)
+#     df_jogos.index += 1
+
+#     st.table(df_jogos)
 
     # Define a função que retorna a planilha em formato XLSX
     def download_excel():
@@ -411,40 +443,40 @@ def pagina_10():
     button = st.download_button(
         label='Download',
         data=download_excel(),
-        file_name=f'LAY_1X0_{dia}.xlsx',
+        file_name=f'Lay_1x0_{dia}.xlsx',
         mime='application/vnd.ms-excel')
-
 
 
 paginas = ['Jogos do Dia',
            'TR - Match Odds', 
-           'TR - Over/Under 0.5FT', 
-           'TR - Over/Under 1.5FT',
-           'TR - Over/Under 2.5FT',
-           'TR - Over/Under 3.5FT',
-           'TR - Over/Under 4.5FT',
+           'TR - Over/Under 2.5FT', 
            'TR - BTTS',
-           'LAY_0X1',
-           'LAY_1X0']
+           'LAY AWAY NEW',
+           'LAY GOLEADA',
+           'BTTS_YES',
+           'OVER05HT',
+           'OVER2_5',
+           'LAY_0X1']
+
 escolha = st.sidebar.radio('',paginas)
 
 if escolha == 'Jogos do Dia':
     pagina_01()
 if escolha == 'TR - Match Odds':
     pagina_02()
-if escolha == 'TR - Over/Under 0.5FT':
-    pagina_03()
-if escolha == 'TR - Over/Under 1.5FT':
-    pagina_04()
 if escolha == 'TR - Over/Under 2.5FT':
-    pagina_05()
-if escolha == 'TR - Over/Under 3.5FT':
-    pagina_06()
-if escolha == 'TR - Over/Under 4.5FT':
-    pagina_07()
+    pagina_03()
 if escolha == 'TR - BTTS':
+    pagina_04()
+if escolha == 'LAY AWAY NEW':
+    pagina_05()
+if escolha == 'LAY GOLEADA':
+    pagina_06()
+if escolha == 'BTTS YES':
+    pagina_07()
+if escolha == 'OVER05HT':
     pagina_08()
-if escolha == 'LAY_0X1':
+if escolha == 'OVER2_5':
     pagina_09()
 if escolha == 'LAY_1X0':
     pagina_10()
