f3272fec392034cf799f835f82ea7cef9210e40d
rpthiago <rp_thiago@yahoo.com.br>
2023-07-28 11:48:38 -0300

    UPDATE

---
+++ jogos_do_dia_transform.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Users\thiag\anaconda3\envs\pycaret\python.exe" "C:\Users\thiag\OneDrive\curso python\modelo_leandro_2\jogos_dia_transform(last)_amanha_atualizado_07_23.py"
+exit
+
---
+++ webscraping_dia.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Users\thiag\anaconda3\envs\pycaret\python.exe" "C:\Users\thiag\OneDrive\curso python\modelo_leandro_2\flashscore\WebScraping_FlashScore_Jogos_do_Dia_Amanha.py"
+exit
+
