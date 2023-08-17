4725b0e1b01cfdc1de3a0a06a8696676e2cf4d20
rpthiago <rp_thiago@yahoo.com.br>
2023-08-17 20:57:48 -0300

    update

--- app.py
+++ app.py
@@ -6,48 +6,9 @@ import xlsxwriter
 
 st.set_page_config(
     page_title='Teoria do Retornos - Grupo Arkad - FLASHSCORE',
-)
+)  
 
 def pagina_01():
-    st.subheader("Jogos do Dia")
-
-    dia = st.date_input(
-        "Data de Análise",
-        date.today())
-
-    ########## Importando os Jogos do Dia ##########
-
-    @st.cache
-    def load_data_jogos():
-        data_jogos = pd.read_csv(f"./Jogos_do_Dia/{dia}_Jogos_do_Dia.csv")
-        
-        return data_jogos
-
-    df_jogos = load_data_jogos()
-
-    df_jogos.dropna(inplace=True)
-    df_jogos = df_jogos.reset_index(drop=True)
-    df_jogos.index += 1
-
-    st.dataframe(df_jogos)
-
-    # Define a função que retorna a planilha em formato XLSX
-    def download_excel():
-        output = BytesIO()
-        writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        df_jogos.to_excel(writer, index=False, sheet_name='Sheet1')
-        writer.save()
-        processed_data = output.getvalue()
-        return processed_data
-
-    # Cria o botão de download
-    button = st.download_button(
-        label='Download',
-        data=download_excel(),
-        file_name=f'Jogos_do_Dia_{dia}.xlsx',
-        mime='application/vnd.ms-excel')   
-
-def pagina_02():
     st.subheader("Teoria dos Retornos - Match Odds")
 
     dia = st.date_input(
@@ -88,7 +49,7 @@ def pagina_02():
     
 
 
-def pagina_03():
+def pagina_02():
     st.subheader("Teoria dos Retornos - Over/Under 2.5 FT")
 
     dia = st.date_input(
@@ -127,7 +88,7 @@ def pagina_03():
         file_name=f'Teoria_dos_Retornos_Over_Under_25_ARKAD_{dia}.xlsx',
         mime='application/vnd.ms-excel')
     
-def pagina_04():
+def pagina_03():
     st.subheader("Teoria dos Retornos - Over/Under 0.5 FT")
 
     dia = st.date_input(
@@ -166,7 +127,7 @@ def pagina_04():
         file_name=f'Teoria_dos_Retornos_Over_Under_05ft_ARKAD_{dia}.xlsx',
         mime='application/vnd.ms-excel')
     
-def pagina_05():
+def pagina_04():
     st.subheader("Teoria dos Retornos - Over/Under 4.5 FT")
 
     dia = st.date_input(
@@ -205,7 +166,7 @@ def pagina_05():
         file_name=f'Teoria_dos_Retornos_Over_Under_45ft_ARKAD_{dia}.xlsx',
         mime='application/vnd.ms-excel')    
 
-def pagina_06():
+def pagina_05():
     st.subheader("Teoria dos Retornos - Ambas Marcam")
 
     dia = st.date_input(
@@ -245,7 +206,7 @@ def pagina_06():
         mime='application/vnd.ms-excel')
     
 
-def pagina_07():
+def pagina_06():
     st.subheader("LAY AWAY NEW")
 
     dia = st.date_input(
@@ -286,7 +247,7 @@ def pagina_07():
     
 
     
-def pagina_08():
+def pagina_07():
     st.subheader("Lay Goleada")
 
     dia = st.date_input(
@@ -325,7 +286,7 @@ def pagina_08():
         file_name=f'lay_goleada_{dia}.xlsx',
         mime='application/vnd.ms-excel')
     
-def pagina_09():
+def pagina_08():
     st.subheader("BTTS_YES")
 
     dia = st.date_input(
@@ -365,7 +326,7 @@ def pagina_09():
         mime='application/vnd.ms-excel')
 
 
-def pagina_10():
+def pagina_09():
     st.subheader("LAY 0 X 1")
 
     dia = st.date_input(
@@ -404,7 +365,7 @@ def pagina_10():
         file_name=f'lay0x1_{dia}.xlsx',
         mime='application/vnd.ms-excel')
 
-def pagina_11():
+def pagina_10():
     st.subheader("OVER2_5")
 
     dia = st.date_input(
@@ -443,7 +404,7 @@ def pagina_11():
         file_name=f'OVER2_5_{dia}.xlsx',
         mime='application/vnd.ms-excel')
 
-def pagina_12():
+def pagina_11():
     st.subheader("Lay_1x0")
 
     dia = st.date_input(
@@ -482,7 +443,7 @@ def pagina_12():
         file_name=f'Lay_1x0_{dia}.xlsx',
         mime='application/vnd.ms-excel')    
 
-def pagina_13():
+def pagina_12():
     st.subheader("lay_home_new")
 
     dia = st.date_input(
@@ -521,7 +482,7 @@ def pagina_13():
         file_name=f'lay_home_new_{dia}.xlsx',
         mime='application/vnd.ms-excel')       
 
-def pagina_14():
+def pagina_13():
     st.subheader("over05ht")
 
     dia = st.date_input(
@@ -561,7 +522,7 @@ def pagina_14():
         mime='application/vnd.ms-excel') 
     
     
-def pagina_15():
+def pagina_14():
     st.subheader("under4_5")
 
     dia = st.date_input(
@@ -600,7 +561,7 @@ def pagina_15():
         mime='application/vnd.ms-excel')
     
     
-def pagina_16():
+def pagina_15():
     st.subheader("lay_away")
 
     dia = st.date_input(
@@ -641,7 +602,7 @@ def pagina_16():
         file_name=f'lay_away_{dia}.xlsx',
         mime='application/vnd.ms-excel')
     
-def pagina_17():
+def pagina_16():
     st.subheader("AI_LAY_AWAY")
 
     dia = st.date_input(
@@ -682,7 +643,7 @@ def pagina_17():
         file_name=f'AI_LAY_AWAY_{dia}.xlsx',
         mime='application/vnd.ms-excel')
 
-def pagina_18():
+def pagina_17():
     st.subheader("Lay_0x2_EVENTOS_RAROS")
 
     dia = st.date_input(
@@ -723,7 +684,7 @@ def pagina_18():
 
    
     
-def pagina_19():
+def pagina_18():
     st.subheader("LAY_GOLEADA_H_EVENTOS_RAROS")
 
     dia = st.date_input(
@@ -763,7 +724,7 @@ def pagina_19():
         mime='application/vnd.ms-excel') 
     
     
-def pagina_20():
+def pagina_19():
     st.subheader("LAY_GOLEADA_A_EVENTOS_RAROS")
 
     dia = st.date_input(
@@ -802,7 +763,7 @@ def pagina_20():
         file_name=f'LAY_GOLEADA_A_EVENTOS_RAROS_{dia}.xlsx',
         mime='application/vnd.ms-excel')    
 
-def pagina_21():
+def pagina_20():
     st.subheader("Lay_0x2_EVENTOS_RAROS")
 
     dia = st.date_input(
@@ -841,7 +802,7 @@ def pagina_21():
         file_name=f'Lay_0x2_EVENTOS_RAROS_{dia}.xlsx',
         mime='application/vnd.ms-excel')    
 
-def pagina_22():
+def pagina_21():
     st.subheader("Lay_2x0_EVENTOS_RAROS")
 
     dia = st.date_input(
@@ -880,7 +841,7 @@ def pagina_22():
         file_name=f'Lay_2x0_EVENTOS_RAROS_{dia}.xlsx',
         mime='application/vnd.ms-excel')    
 
-def pagina_23():
+def pagina_22():
     st.subheader("Lay_2x2_EVENTOS_RAROS")
 
     dia = st.date_input(
@@ -919,7 +880,7 @@ def pagina_23():
         file_name=f'Lay_2x2_EVENTOS_RAROS_{dia}.xlsx',
         mime='application/vnd.ms-excel')    
 
-def pagina_24():
+def pagina_23():
     st.subheader("Lay_1x3_EVENTOS_RAROS")
 
     dia = st.date_input(
@@ -958,7 +919,7 @@ def pagina_24():
         file_name=f'Lay_1x3_EVENTOS_RAROS_{dia}.xlsx',
         mime='application/vnd.ms-excel')    
 
-def pagina_25():
+def pagina_24():
     st.subheader("Lay_3x1_EVENTOS_RAROS")
 
     dia = st.date_input(
@@ -997,8 +958,7 @@ def pagina_25():
         file_name=f'Lay_3x1_EVENTOS_RAROS_{dia}.xlsx',
         mime='application/vnd.ms-excel')     
     
-paginas = ['Jogos do Dia',
-           'TR - Match Odds', 
+paginas = ['TR - Match Odds', 
            'TR - Over/Under 2.5FT',
            'TR - Over/Under 0.5FT',
            'TR - Over/Under 4.5FT',
@@ -1025,53 +985,51 @@ paginas = ['Jogos do Dia',
 
 escolha = st.sidebar.radio('',paginas)
 
-if escolha == 'Jogos do Dia':
-    pagina_01()
 if escolha == 'TR - Match Odds':
-    pagina_02()
+    pagina_01()
 if escolha == 'TR - Over/Under 2.5FT':
-    pagina_03()
+    pagina_02()
 if escolha == 'TR - Over/Under 0.5FT':
-    pagina_04()    
+    pagina_03()    
 if escolha == 'TR - Over/Under 4.5FT':
-    pagina_05()          
+    pagina_04()          
 if escolha == 'TR - BTTS':
-    pagina_06()
+    pagina_05()
 if escolha == 'LAY AWAY NEW':
-    pagina_07()
+    pagina_06()
 if escolha == 'LAY GOLEADA':
-    pagina_08()
+    pagina_07()
 if escolha == 'BTTS_YES':
-    pagina_09()
+    pagina_08()
 if escolha == 'LAY 0 X 1':
-    pagina_10()
+    pagina_09()
 if escolha == 'OVER2_5':
-    pagina_11()
+    pagina_10()
 if escolha == 'Lay_1x0':
-    pagina_12()
+    pagina_11()
 if escolha == 'lay_home_new':
-    pagina_13()
+    pagina_12()
 if escolha == 'over05ht':
-    pagina_14()
+    pagina_13()
 if escolha == 'under4_5':
-    pagina_15()
+    pagina_14()
 if escolha == 'lay_away':
-    pagina_16()
+    pagina_15()
 if escolha == 'AI_LAY_AWAY':
-    pagina_17()    
+    pagina_16()    
 if escolha == 'Lay_0x2_EVENTOS_RAROS':
-    pagina_18()     
+    pagina_17()     
 if escolha == 'LAY_GOLEADA_H_EVENTOS_RAROS':
-    pagina_19()     
+    pagina_18()     
 if escolha == 'LAY_GOLEADA_A_EVENTOS_RAROS':
-    pagina_20()
+    pagina_19()
 if escolha == 'Lay_0x2_EVENTOS_RAROS':
-    pagina_21()    
+    pagina_20()    
 if escolha == 'Lay_2x0_EVENTOS_RAROS':
-    pagina_22()    
+    pagina_21()    
 if escolha == 'Lay_2x2_EVENTOS_RAROS':
-    pagina_23()    
+    pagina_22()    
 if escolha == 'Lay_1x3_EVENTOS_RAROS':
-    pagina_24()     
+    pagina_23()     
 if escolha == 'Lay_3x1_EVENTOS_RAROS':
-    pagina_25()           
+    pagina_24()           
